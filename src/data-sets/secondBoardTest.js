aa8f2b026271d07a1a67a0a102c07c2856c7b7cb
Germ-X <mrxmc1991@gmail.com>
2025-03-21 13:07:42 -0500

    Add files via upload

---
+++ src/data-sets/secondBoardTest.js
@@ -0,0 +1,186 @@
+
+export const secondBoardTestDataSet = {
+    name: 'example',
+    columns: [
+        {
+            name: 'Listening to the Classics',
+            questions: [
+                {
+                    value: '$400',
+                    question: 'Q',
+                    answer: "A"
+                },
+                {
+                    value: '$800',
+                    question: 'Q',
+                    answer: "A"
+                },
+                {
+                    value: '$1200',
+                    question: 'Q',
+                    answer: "A"
+                },
+                {
+                    value: '$1600',
+                    question: 'Q',
+                    answer: "A"
+                },
+                {
+                    value: '$2000',
+                    question: 'Q',
+                    answer: "A"
+                },
+            ],
+        },
+        {
+            name: 'The Madman's Militia',
+            questions: [
+                {
+                    value: '$400',
+                    question: 'This flying bee patrols the Green Hills Sonic is so famous for.',
+                    answer: "Buzzbomber"
+                },
+                {
+                    value: '$800',
+                    question: 'An exploding insect that must be rolled into to defeat it.',
+                    answer: "Caterkiller"
+                },
+                {
+                    value: '$1200',
+                    question: 'A floating sentry with no defensive or offensive capabilities.',
+                    answer: "Flapper"
+                },
+                {
+                    value: '$1600',
+                    question: 'One might say this Badnik is the *star* of Metropolis Zone.',
+                    answer: "Asteron"
+                },
+                {
+                    value: '$2000',
+                    question: 'These robots bear a striking resemblance to a previous Eggman crony.',
+                    answer: "E-1000"
+                },
+            ],
+        },
+        {
+            name: 'Sonic Says',
+            questions: [
+                {
+                    value: '$400',
+                    question: 'No copyright law in the universe is gonna stop me!',
+                    answer: "Sonic the Hedgehog"
+                },
+                {
+                    value: '$800',
+                    question: 'Worthless consumer models!',
+                    answer: "E-123 Omega"
+                },
+                {
+                    value: '$1200',
+                    question: 'Who's this broad?',
+                    answer: "Vector the Crocodile"
+                },
+                {
+                    value: '$1600',
+                    question: 'Five minutes should be plenty!',
+                    answer: "Rouge the Bat"
+                },
+                {
+                    value: '$2000',
+                    question: 'It's my turn now!',
+                    answer: "Amy Rose"
+                },
+            ],
+        },
+        {
+            name: 'Gotta Go Fast!!',
+            questions: [
+                {
+                    value: '$400',
+                    question: 'This Sonic Frontiers speed tech was offically embraced by the developers.',
+                    answer: "Magnet Dash"
+                },
+                {
+                    value: '$800',
+                    question: 'Shadow can use this technique to maintain his speed in midair in Shadow Generations.',
+                    answer: "Spear Gliding"
+                },
+                {
+                    value: '$1200',
+                    question: 'Q',
+                    answer: "A"
+                },
+                {
+                    value: '$1600',
+                    question: 'This intended mechanic is the reason all Genesis Sonic games use a sum of the In-Game Timer to determine your final time.',
+                    answer: "Time Bonus"
+                },
+                {
+                    value: '$2000',
+                    question: 'Casinopolis',
+                    answer: "This Sonic Adventure stage can be beaten in under 10 seconds by clipping through a wall and jumping off of some hidden ground."
+                },
+            ],
+        },
+        {
+            name: 'Oh the Zones you will go...',
+            questions: [
+                {
+                    value: '$400',
+                    question: 'Q',
+                    answer: "A"
+                },
+                {
+                    value: '$800',
+                    question: 'Q',
+                    answer: "A"
+                },
+                {
+                    value: '$1200',
+                    question: 'Q',
+                    answer: "A"
+                },
+                {
+                    value: '$1600',
+                    question: 'Q',
+                    answer: "A"
+                },
+                {
+                    value: '$2000',
+                    question: 'Q',
+                    answer: "A"
+                },
+            ],
+        },
+        {
+            name: 'CODENAME: I.D.W. Incredibly Descriptive Writing',
+            questions: [
+                {
+                    value: '$400',
+                    question: 'The current comic run follows after the events of this Sonic game.',
+                    answer: "Sonic Forces"
+                },
+                {
+                    value: '$800',
+                    question: 'These two characters were the first in the IDW comic run to get their own miniseries.',
+                    answer: "Tangle & Whisper"
+                },
+                {
+                    value: '$1200',
+                    question: 'Dr. Starline created this duo in order to combat and dismantle Sonic & Tails.',
+                    answer: "Surge & Kitsunami"
+                },
+                {
+                    value: '$1600',
+                    question: 'Shadow says "Cowards run, I win!" before falling to this threat.',
+                    answer: "The Metal Virus"
+                },
+                {
+                    value: '$2000',
+                    question: 'This is the first super form seen in the IDW comic run.',
+                    answer: "Burning Blaze"
+                },
+            ],
+        },
+    ],
+};
